cfa8e9c98f5adde2825b002c5766af4270dfcfa9
Stellamauci <mashengwen0532@gmail.com>
2023-02-21 13:49:53 -0800

    Add getCategoryByID and getSubCategory

--- db.js
+++ db.js
@@ -39,4 +39,22 @@ dbWorker.getCategoryByID = (categoryID, callback) => {
     });
 }
 
+dbWorker.getSubCategory = (parentID, callback) => {
+    sql = "SELECT * FROM product_category WHERE parent_id = ?";
+    console.log(parentID);
+    conn.query(sql, [parentID], function (err, result) {
+        if (err) throw err;
+        callback(result[0]);
+    });
+}
+
+
+dbWorker.productsUnderCategory = (categoryID, callback) => {
+    sql = "SELECT * FROM products WHERE category_id = ?";
+    console.log(categoryID);
+    conn.query(sql, [categoryID], function (err, result) {
+        if (err) throw err;
+        callback(result[0]);
+    });
+}
 module.exports = dbWorker;
